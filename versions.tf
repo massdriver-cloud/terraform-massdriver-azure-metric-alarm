656bc019e1469f987a907acef9c9a1562e4c3837
chrisghill <chris@massdriver.cloud>
2026-04-14 17:37:12 -0600

    versions.tf

--- versions.tf
+++ versions.tf
@@ -1,10 +1,14 @@
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     azurerm = {
-      source = "hashicorp/azurerm"
+      source  = "hashicorp/azurerm"
+      version = ">= 4.0"
     }
     massdriver = {
-      source = "massdriver-cloud/massdriver"
+      source  = "massdriver-cloud/massdriver"
+      version = ">= 1.0"
     }
   }
 }
